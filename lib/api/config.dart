4d4d0e33fcc5400f48ecf3a0274c8373d891e7a4
nnnubu <1425739799@qq.com>
2026-09-10 18:24:28 +0800

    docs: 更新readme待开发状态 图片语音视频消息/消息撤回/本地缓存体系标记完成 取消跟踪含公网IP的config

--- lib/api/config.dart
+++
@@ -1,7 +0,0 @@
-class ApiConfig {
-  static const String _baseServer = "192.168.1.9:8080";
-
-  static const String baseUrl = "http://${ApiConfig._baseServer}";
-
-  static const String wsUrl = "ws://${ApiConfig._baseServer}";
-}
